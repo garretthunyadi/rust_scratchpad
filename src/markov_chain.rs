36499c0d6fa1b4a59550ec827767c0d07979f224
Garrett Hunyadi <garrett.hunyadi@endurance.com>
2019-10-19 13:04:26 -0400

    markov

--- src/markov_chain.rs
+++ src/markov_chain.rs
@@ -1,25 +1,94 @@
+use rand::prelude::*;
+use std::collections::HashMap;
+
 pub fn main() -> std::io::Result<()> {
     println!("markov chain");
 
-    let orig_text = include_str!("../data/moby_dick.txt");
+    let orig_text = String::from(include_str!("../data/moby_dick.txt"));
+    // orig_text.truncate(100000);
+    // println!("orig : {:?}", words(&orig_text));
+
     // create one string that we can use slices from
-    let mut corpus = simplify_corpus(orig_text);
+    let corpus = simplify_corpus(&orig_text);
     // println!("words: {:?}", words(&corpus.clone().truncate(100)));
-    corpus.truncate(1000);
-    println!("words: {:?}", words(&corpus));
+    // corpus.truncate(1000);
+    let words = words(&corpus);
+    // println!("words: {:?}", words);
+
+    let first_bigram = (*words.get(0).unwrap(), *words.get(1).unwrap());
+    println!("first_bigram: {:?}", first_bigram);
+
+    let model = bigram_markov_model(&words);
+    // println!("model: {:?}", model);
+    let ws = model.get(&first_bigram).unwrap();
+    println!("ws: {:?}", ws);
+    let w = *ws.choose(&mut rand::thread_rng()).unwrap();
+    println!("rand(ws): {:?}", w);
 
     Ok(())
+}
+
+type Bigram<'a> = (&'a str, &'a str);
+type Trigram<'a> = (&'a str, &'a str, &'a str);
+
+type BigramMarkovModel<'a> = HashMap<Bigram<'a>, Vec<&'a str>>;
+
+pub fn bigram_markov_model<'a>(words: &[&'a str]) -> BigramMarkovModel<'a> {
+    let mut model = BigramMarkovModel::new();
+
+    let mut last_word = "";
+    let mut last_last_word = "";
+    for word in words {
+        // process the new word
+        update_model(&mut model, &(last_last_word, last_word, word));
+        last_last_word = last_word;
+        last_word = word;
+    }
+    model
+}
+
+#[test]
+fn test_bigram_markov_model() {
+    // unimplemented!();
+}
+
+fn update_model<'a>(model: &mut BigramMarkovModel<'a>, trigram: &Trigram<'a>) {
+    // println!("update_model:{:?}", trigram);
+    let bigram = (trigram.0, trigram.1);
+    let word = trigram.2;
+
+    // model.insert(bigram, vec![trigram.2]);
+    // let words = model.get_mut(&bigram).unwrap();
+    // words.push(trigram.2)
+
+    match model.get_mut(&bigram) {
+        Some(words) => {
+            words.push(word);
+        }
+        None => {
+            model.insert(bigram, vec![word]);
+        }
+    }
+    // function chain(dict::BigramMarkovChain, (w1, w2, w3))
+    //     k = (w1, w2)
+    //     if (haskey(dict, k))
+    //         dict[k] = append!(dict[k], [w3])
+    //     else
+    //         dict[k] = [w3]
+    //     end
+    //     dict
+    // end
 }
 
 pub fn simplify_corpus(text: &str) -> String {
     text.split_ascii_whitespace()
         .map(|word| {
-            println!("{}", word);
             word.chars()
                 .filter(|&c| c.is_alphanumeric())
                 .collect::<String>()
         })
-        .collect::<Vec<String>>().join(" ")
+        .collect::<Vec<String>>()
+        .join(" ")
 }
 #[test]
 fn test_simplify_corpus() {
@@ -44,17 +113,5 @@ fn test_simplify_word() {
 }
 
 pub fn words(text: &str) -> Vec<&str> {
-    text.split_ascii_whitespace()
-        // .map(|word| word.replace(".", ""))
-        .collect()
+    text.split_ascii_whitespace().collect()
 }
-
-// #[test]
-// fn test_words() {
-//     assert_eq!(words(&"some words here"), vec!["some", "words", "here"]);
-//     assert_eq!(words(&"s.ome word.s here."), vec!["some", "words", "here"]);
-//     assert_eq!(
-//         words(&"s.ome\t2word.s here."),
-//         vec!["some", "2words", "here"]
-//     );
-// }
