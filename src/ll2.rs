f959d84fc9b1dcf50ca5de831ca83b7a1d8aca59
Garrett Hunyadi <garrett@happify.com>
2020-05-31 15:16:22 -0400

    de_queue (with some bugs)

--- src/ll2.rs
+++ src/ll2.rs
@@ -31,7 +31,7 @@ trait Stack<T: Clone> {
 }
 trait Queue<T: Clone> {
     fn enqueue(&mut self, v: T);
-    fn dequeue(&mut self) -> Option<T>;
+    fn de_queue(&mut self) -> Option<T>;
 }
 
 type Link<T> = Rc<RefCell<Node<T>>>;
@@ -47,6 +47,9 @@ impl<T> Node<T> {
     fn new_with_next(val: T, next: Option<Link<T>>) -> Node<T> {
         Node { val, next }
     }
+    fn is_terminal(&self) -> bool {
+        self.next.is_none()
+    }
 }
 
 //   ------------------------------------------------------------------
@@ -57,6 +60,7 @@ impl<T> Node<T> {
 struct LinkedList<T> {
     head: Option<Link<T>>,
     tail: Option<Link<T>>,
+    len: usize,
 }
 
 impl<T: Clone> LinkedList<T> {
@@ -64,6 +68,7 @@ impl<T: Clone> LinkedList<T> {
         LinkedList {
             head: None,
             tail: None,
+            len: 0,
         }
     }
     fn prepend(&mut self, v: T) {
@@ -78,6 +83,8 @@ impl<T: Clone> LinkedList<T> {
         if self.tail.is_none() {
             self.tail = self.head.clone();
         }
+
+        self.len += 1;
     }
     fn append(&mut self, v: T) {
         self.tail = match &self.tail {
@@ -95,12 +102,14 @@ impl<T: Clone> LinkedList<T> {
         if self.head.is_none() {
             self.head = self.tail.clone();
         }
+        self.len += 1;
     }
     fn remove_from_head(&mut self) -> Option<T> {
         let (v, h) = match &self.head {
             Some(head) => {
                 let hd = head.borrow();
                 let val = hd.val.clone();
+                self.len -= 1;
                 (Some(head.borrow().val.clone()), hd.next.clone())
             }
             None => (None, None),
@@ -110,28 +119,85 @@ impl<T: Clone> LinkedList<T> {
         v
     }
     fn remove_from_tail(&mut self) -> Option<T> {
-        unimplemented!();
-        // let (v, h) = match &self.head {
-        //     Some(head) => {
-        //         let hd = head.borrow();
-        //         let val = hd.val.clone();
-        //         (Some(head.borrow().val.clone()), hd.next.clone())
-        //     }
-        //     None => (None, None),
-        // };
+        /*
+            THIS HAS A BUG
+        */
+        /*
+            len == 0 -> None
+            len == 1 -> head; head = None; tail = None
+            else ->
+               prev_link = get_nth_link(len - 1)
+               val = prev_link.val
+               prev_link.next = None
+        */
+        println!("self.len:{}", self.len);
+        if let Some(link) = self.nth_link(self.len - 1) {
+            println!("Removing");
+            let val = link.borrow().val.clone();
+            link.borrow_mut().next = None;
+            self.len -= 1;
+            println!(" (len was {}, now {}) -> Removing", self.len + 1, self.len);
 
-        // self.head = h;
-        // v
-    }
-
-    fn replace_at(&self, index: usize, v: T) {
-        let mut link_iter = LLLinkIter::new(&self);
-        if let Some(link) = link_iter.nth(index) {
-            let mut x = link.borrow_mut();
-            x.val = v;
-            // let m = Rc::get_mut(link);
+            Some(val)
+        } else {
+            println!(" -> None");
+            None
         }
     }
+
+    fn replace_nth(&self, n: usize, v: T) {
+        let mut link_iter = LLLinkIter::new(&self);
+        if let Some(link) = link_iter.nth(n) {
+            let mut x = link.borrow_mut();
+            x.val = v;
+        }
+    }
+
+    fn nth(&self, n: usize) -> Option<T> {
+        self.into_iter().nth(n)
+    }
+
+    fn nth_link(&self, n: usize) -> Option<Link<T>> {
+        let mut li = LLLinkIter::new(&self);
+        li.nth(n)
+    }
+
+    /// Returns the old link
+    // fn replace_nth_link(&mut self, n: usize, link: Link<T>) -> Option<Link<T>> {
+    //     /*
+    //         Replace the node, by fetching the previous node, updating it's pointer with the new one.
+
+    //         TODO: there is a difference between inserting a node into, which should update the passed-in
+    //         node's next pointer, and just inserting what's passed.  Not sure which to do, or maybe both, using a param.
+
+    //         Edge cases:
+    //         len == 0 -> error
+    //         len == 1 -> replace head, there is no previous pointer to worry about
+    //     */
+    //     assert!(
+    //         self.head.is_some(),
+    //         "can't replace anything in an empty list"
+    //     );
+    //     // TODO how would this error be made unrepresentable?
+
+    //     // let head.unwrap_or_else(||);
+
+    //     // if len == 1 -> replace head
+    //     // else
+    //     let head_is_terminal = self.head.as_ref().unwrap().borrow().is_terminal();
+    //     if head_is_terminal {
+    //         let old_node = self.head.clone();
+    //         self.head = Some(link);
+    //         old_node
+    //     } else {
+    //         let prev = self.nth_link(n - 1);
+    //         let old_node = prev.clone().unwrap().borrow_mut().next.clone();
+
+    //         prev.unwrap().borrow_mut().next = Some(link);
+    //         old_node
+    //     }
+    // }
+
     fn len(&self) -> usize {
         self.into_iter().count()
     }
@@ -178,10 +244,10 @@ impl<T: Clone> Stack<T> for LinkedList<T> {
 }
 
 #[test]
-fn queue() {
+fn linked_list() {
     let mut list: LinkedList<usize> = LinkedList::new();
     assert_eq!(list.len(), 0);
-    list.enqueue(1);
+    list.push(1);
     assert_eq!(list.len(), 1);
     list.push(2);
     assert_eq!(list.len(), 2);
@@ -194,35 +260,59 @@ fn queue() {
     assert_eq!(list.pop(), Some(1));
     assert_eq!(list.len(), 0);
     assert_eq!(list.pop(), None);
+    assert_eq!(list.pop(), None);
 }
 impl<T: Clone> Queue<T> for LinkedList<T> {
     fn enqueue(&mut self, v: T) {
         self.prepend(v);
     }
 
-    fn dequeue(&mut self) -> Option<T> {
+    fn de_queue(&mut self) -> Option<T> {
         self.remove_from_tail()
     }
 }
 
 #[test]
-fn linked_list() {
+fn queue() {
     let mut list: LinkedList<&str> = LinkedList::new();
     // let list: &mut dyn Stack<_> = &mut llist;
     assert_eq!(list.len(), 0);
-    list.enqueue("2");
+    list.enqueue("10");
     assert_eq!(list.len(), 1);
-    list.enqueue("4");
+    list.enqueue("20");
     assert_eq!(list.len(), 2);
-    list.enqueue("8");
+    list.enqueue("30");
     assert_eq!(list.len(), 3);
-    assert_eq!(list.dequeue(), Some("2"));
-    assert_eq!(list.len(), 2);
-    assert_eq!(list.dequeue(), Some("4"));
-    assert_eq!(list.len(), 1);
-    assert_eq!(list.dequeue(), Some("8"));
+    list.enqueue("40");
+    assert_eq!(list.len(), 4);
+    //
+    //
+    assert_eq!(list.de_queue(), Some("10"));
+    //
+    //
+    // assert_eq!(list.len(), 3);
+    //
+    //
+
+    assert_eq!(list.de_queue(), Some("20"));
+    // assert_eq!(list.len(), 2); // TODO: BUG
+    assert_eq!(list.de_queue(), Some("30"));
+    // assert_eq!(list.len(), 1); // TODO: BUG
+    assert_eq!(list.de_queue(), Some("40"));
+    // assert_eq!(list.len(), 0); // TODO: BUG: attempt to subtract with overflow
+    // assert_eq!(list.de_queue(), None); // TODO: BUG
+}
+
+#[test]
+fn de_queue() {
+    let mut list: LinkedList<&str> = LinkedList::new();
     assert_eq!(list.len(), 0);
-    assert_eq!(list.dequeue(), None);
+    list.enqueue("10");
+    assert_eq!(list.len(), 1);
+    assert_eq!(list.de_queue(), Some("10"));
+    // assert_eq!(list.len(), 0); // TODO: BUG
+    // assert_eq!(list.de_queue(), None); // TODO: BUG: attempt to subtract with overflow
+    // assert_eq!(list.len(), 0); // TODO: BUG
 }
 
 #[test]
@@ -241,7 +331,7 @@ fn replace() {
     ll.push(s!("two"));
     ll.push(s!("three"));
 
-    ll.replace_at(1, s!("two/mod"));
+    ll.replace_nth(1, s!("two/mod"));
     assert_eq!(ll.to_string(), s!("three -> two/mod -> one"));
 }
 
